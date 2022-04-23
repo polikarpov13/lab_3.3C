9d36f5dea98388fcf892a6c216822d5449fe4ba7
{Vlad} <{vladpolikarpovvl@gmail.com}>
2022-04-23 14:26:56 +0300

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../../../../політех/ооп/3/lab_3.3C/lab_3.3C/Object.cpp"
+#include "../../../../../політех/ооп/3/lab_3.3C/lab_3.3C/LongLong.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			LongLong A(4, 4), B(4, 4);
+			Assert::AreEqual(A == B, true);
+		}
+	};
+}
---
+++ lab_3.3C/lab_3.3C.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream> 
+
+#include <Windows.h>  
+
+#include "LongLong.h" 
+
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    LongLong A;
+    LongLong B;
+
+    cout << " Введіть A числа: " << endl;
+    cin >> A;
+
+    cout << " Кількість елементів класу LongLong: " << Object::Count() << endl;
+
+    cout << " Введіть B числа: " << endl;
+    cin >> B;
+
+    cout << " Кількість елементів класу LongLong: " << Object::Count() << endl;
+
+    cout << " A + B: " << endl;
+    cout << A + B << endl;
+
+    cout << " Кількість елементів класу LongLong: " << Object::Count() << endl;
+
+    cout << " A * B:" << endl;
+
+    cout << A * B << endl;
+
+    cout << " Кількість елементів класу LongLong: " << Object::Count() << endl;
+
+
+
+    cout << " A > B " << endl;
+    if (A > B) cout << " Yes " << endl; else cout << " No " << endl;
+
+    cout << " A < B " << endl;
+    if (A < B) cout << " Yes " << endl; else cout << " No " << endl;
+
+    cout << " A == B " << endl;
+    if (A == B) cout << " Yes " << endl; else cout << " No " << endl;
+
+}
